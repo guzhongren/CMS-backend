aa855497eec7d9e65195390fa21d893671c737e6
guzhongren <guzhongren@live.cn>
2019-03-01 07:57:28 +0800

    continue(db): 使用db进行用户查询

--- user.go
+++ user.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -9,24 +10,29 @@ import (
 type User struct {
 	Id     int    `json: "id`
 	Name   string `json: "name"`
-	RoleId string `json: "roleid"`
+	RoleId string `json: "roleId"`
 }
 
 func (user User) getAllUser(db *sql.DB) ([]User, error) {
-	rows, err := db.Query("select * from b_user")
+	rows, err := db.Query("select name from b_user")
 	if err != nil {
 		log.Warn("查询出错")
 		return nil, err
 	}
 	var userList = []User{}
+	var nameList = []string{}
+	log.Info(rows)
+	fmt.Printf("%v", rows)
 	for rows.Next() {
-		user := User{}
-		err := rows.Scan(&user.Name, &user.RoleId, &user.Id)
+		// user := User{}
+		var name string
+		err := rows.Scan(&name)
+
 		if err != nil {
-			log.Warn("处理查询结果出错")
+			log.Warn("处理查询结果出错", err)
 			return nil, err
 		}
-		userList = append(userList, user)
+		nameList = append(nameList, name)
 	}
 	return userList, nil
 
