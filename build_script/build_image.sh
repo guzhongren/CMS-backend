4d6fd9b058ffada0a735ace4cc6f0e31ff9ca935
guzhongren <guzhongren@live.cn>
2019-06-09 19:11:49 +0800

    ci(build_image.sh): 优化

--- build_script/build_image.sh
+++ build_script/build_image.sh
@@ -4,8 +4,13 @@ HUB_DOMAIN=$1
 DOCKERUSER=$2
 DOCKERPASSWORD=$3
 PROJECT=$4
-APP=$5
+APP=$5i
 
+echo "私有仓库域: ${HUB_DOMAIN}"
+echo "docker 用户名: ${DOCKERUSER}"
+echo "docker 密码: ******"
+echo "项目名称: ${PROJECT}"
+echo "应用名称: ${APP}"
 TIME=`date "+%Y%m%d%H%M"`
 GIT_REVISION=`git log -1 --pretty=format:"%H"`
 # IMAGE_NAME=hub.k8s.com:8080/cms/${MODULE}:${TIME}_${GIT_REVISION}
--- build_script/deploy_image.sh
+++ build_script/deploy_image.sh
@@ -3,7 +3,7 @@
 DEPLOYMENT=$1
 APP=$2
 IMAGE=`cat image_name`
-echo "镜像名称：${IMAGE_NAME}"
+echo "镜像名称：${IMAGE}"
 echo "要更新的镜像:${APP}"
 echo "要更新的 Deployment:${DEPLOYMENT}"
 # Kubectl set image delplyments/${DEPLOYMENT} ${APP}=${IMAGE}
