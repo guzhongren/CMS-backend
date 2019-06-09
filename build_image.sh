af9d0358487ebe9fc3f9953d944c89369dff3e7a
guzhongren <guzhongren@live.cn>
2019-06-09 10:15:58 +0800

    ci(build_image): 新增脚本

--- build_image.sh
+++ build_image.sh
@@ -4,7 +4,7 @@ MODULE=$1
 TIME=`date "+%Y%m%d%H%M"`
 GIT_REVISION=`git log -1 --pretty=format:"%H"`
 # IMAGE_NAME=hub.k8s.com:8080/cms/${MODULE}:${TIME}_${GIT_REVISION}
-IMAGE_NAME={MODULE}:${TIME}_${GIT_REVISION}
+IMAGE_NAME=${MODULE}:${TIME}_${GIT_REVISION}
 
 # cd ${MODULE}
 
