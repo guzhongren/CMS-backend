cf58df95e371216407c02419e7edc0d6f12050c0
guzhongren <guzhongren@live.cn>
2019-06-21 07:37:21 +0800

    fix(script): 修复部署脚本

--- build_script/deploy_image.sh
+++ build_script/deploy_image.sh
@@ -6,4 +6,4 @@ IMAGE=`cat image_name`
 echo "镜像名称：${IMAGE}"
 echo "要更新的镜像:${APP}"
 echo "要更新的 Deployment:${DEPLOYMENT}"
-# Kubectl set image delplyments/${DEPLOYMENT} ${APP}=${IMAGE}
+kubectl set image delplyments/${DEPLOYMENT} ${APP}=${IMAGE}
