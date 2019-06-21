5bdf705003d6563c483efd155633a6dd75db0c60
guzhongren <guzhongren@live.cn>
2019-06-21 20:04:59 +0800

    pref(ci): 更新ci

--- build_script/deploy_image.sh
+++ build_script/deploy_image.sh
@@ -6,4 +6,4 @@ IMAGE=`cat image_name`
 echo "镜像名称：${IMAGE}"
 echo "要更新的容器:${APP}"
 echo "要更新的 Deployment:${DEPLOYMENT}"
-kubectl set image delplyments/${DEPLOYMENT} ${APP}=${IMAGE}
+# kubectl set image delplyments/${DEPLOYMENT} ${APP}=${IMAGE}
