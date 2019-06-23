62c40fd67abfd67a40c54cf110cb96e64058de2f
guzhongren <guzhongren@live.cn>
2019-06-23 11:52:51 +0800

    pref(script): 优化脚本

--- build_script/deploy_image.sh
+++ build_script/deploy_image.sh
@@ -9,7 +9,5 @@ echo "镜像名称：${IMAGE}"
 echo "要更新的容器:${CONTAINER}"
 echo "要更新的 Deployment:${DEPLOYMENT}"
 echo "namespace: ${NAMESPACE}"
-echo "测试kubectl是否可用"
-kubectl --kubeconfig /root/.kube/config  get nodes
-echo "测试结束"
+echo "开始更新镜像"
 kubectl --kubeconfig /root/.kube/config -n ${NAMESPACE} set image deployment/${DEPLOYMENT} "${CONTAINER}"=${IMAGE}
