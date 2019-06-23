95f06f4c8d303bdb1029a0d3bc201e573956edd3
guzhongren <guzhongren@live.cn>
2019-06-23 10:27:06 +0800

    test(kubectl): 测试kubectl 是否可用

--- build_script/deploy_image.sh
+++ build_script/deploy_image.sh
@@ -6,4 +6,7 @@ IMAGE=`cat image_name`
 echo "镜像名称：${IMAGE}"
 # echo "要更新的容器:${APP}"
 echo "要更新的 Deployment:${DEPLOYMENT}"
-kubectl set image delplyments/${DEPLOYMENT} "${DEPLOYMENT}-container"=${IMAGE}
+echo "测试kubectl是否可用"
+kubectl --kubeconfig .kube/config  get nodes
+echo "测试结束"
+kubectl --kubeconfig .kube/config set image delplyments/${DEPLOYMENT} "${DEPLOYMENT}-container"=${IMAGE}
