55dacf4945aaa59a157a1e0e28d0da4a22f95b06
guzhongren <guzhongren@live.cn>
2019-06-23 11:11:55 +0800

    fix(shell): 修复拼写错误

--- build_script/deploy_image.sh
+++ build_script/deploy_image.sh
@@ -6,9 +6,7 @@ IMAGE=`cat image_name`
 echo "镜像名称：${IMAGE}"
 # echo "要更新的容器:${APP}"
 echo "要更新的 Deployment:${DEPLOYMENT}"
-echo "测试kubectl 命令是否可用"
-kubectl
 echo "测试kubectl是否可用"
 kubectl --kubeconfig /root/.kube/config  get nodes
 echo "测试结束"
-kubectl --kubeconfig /root/.kube/config set image delplyments/${DEPLOYMENT} "${DEPLOYMENT}-container"=${IMAGE}
+kubectl --kubeconfig /root/.kube/config set image deployment/${DEPLOYMENT} "${DEPLOYMENT}-container"=${IMAGE}
