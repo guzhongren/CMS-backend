d1ab90db84974f154c332ebb54e6530d2e62c4e2
guzhongren <guzhongren@live.cn>
2019-06-23 10:41:36 +0800

    fix(path): 修复kubeconfig 路径问题

--- build_script/deploy_image.sh
+++ build_script/deploy_image.sh
@@ -9,6 +9,6 @@ echo "要更新的 Deployment:${DEPLOYMENT}"
 echo "测试kubectl 命令是否可用"
 kubectl
 echo "测试kubectl是否可用"
-kubectl --kubeconfig ~/.kube/config  get nodes
+kubectl --kubeconfig /root/.kube/config  get nodes
 echo "测试结束"
-kubectl --kubeconfig ~/.kube/config set image delplyments/${DEPLOYMENT} "${DEPLOYMENT}-container"=${IMAGE}
+kubectl --kubeconfig /root/.kube/config set image delplyments/${DEPLOYMENT} "${DEPLOYMENT}-container"=${IMAGE}
