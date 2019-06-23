d2de72afe14d90e690caf089d12d30b99d743fec
guzhongren <guzhongren@live.cn>
2019-06-23 11:35:53 +0800

    fix(selector): 修复selector

--- build_script/deploy_image.sh
+++ build_script/deploy_image.sh
@@ -9,4 +9,4 @@ echo "要更新的 Deployment:${DEPLOYMENT}"
 echo "测试kubectl是否可用"
 kubectl --kubeconfig /root/.kube/config  get nodes
 echo "测试结束"
-kubectl --kubeconfig /root/.kube/config set image deployment/${DEPLOYMENT} "${DEPLOYMENT}-container"=${IMAGE}
+kubectl --kubeconfig /root/.kube/config set image deployment/${DEPLOYMENT} "${DEPLOYMENT}"=${IMAGE}
